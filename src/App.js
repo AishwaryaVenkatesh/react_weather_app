d898dce7e357f025d9f9fac639e94eb92dd015df
Aishwarya <33971260+AishwaryaVenkatesh@users.noreply.github.com>
2022-12-22 22:20:29 -0800

    Added Open source link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <h1> Weather app </h1>;
+  return (
+    <div className="Weatherapp">
+      <h1> Weather app </h1>
+      <p>
+        This project was coded by{" "}
+        <a
+          href="https://aishwaryavenkatesh.github.io/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Aishwarya
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/AishwaryaVenkatesh/react_weather_app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open Sourced
+        </a>{" "}
+      </p>
+    </div>
+  );
 }
